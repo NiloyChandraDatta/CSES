e4158d4bd41f06dff8d2b6f60c0f572c15745753
Niloy Chandra Datta <niloycd98@gmail.com>
2025-11-23 00:23:20 +0600

    Create Trailing Zeros.cpp

---
+++ Trailing Zeros.cpp
@@ -0,0 +1,21 @@
+// https://cses.fi/problemset/task/1618
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+int main()
+{
+    ll n;
+    cin >> n;
+
+    ll cnt = 0;
+    while(n > 0)
+    {
+        cnt += n / 5;
+        n /= 5;
+    }
+
+    cout << cnt << endl;
+
+    return 0;
+}
